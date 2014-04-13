7058558fa5a6b0e0ee1172ce4a3760fb35b3b2b0
at1as <jason@willems.ca>
2014-04-13 19:30:55 -0400

    Initial commit

---
+++ rpn-calc-engine.js
@@ -0,0 +1,233 @@
+var inputBuffer = new Array;
+var currentInput = "";
+var constantEntered = false;
+
+function addToBuffer(keyPress) {
+	if (currentInput == "" && keyPress.innerHTML == 0) { }
+	else {
+		currentInput += keyPress.innerHTML;
+		display.innerHTML = currentInput;
+	}
+}
+
+function addToBufferfromKey(keyInput) {
+	currentInput += keyInput;
+	display.innerHTML = currentInput;
+}
+
+function addToStack() {
+	if (currentInput != "" && currentInput != NaN) {
+		inputBuffer.push(currentInput);
+		stackDisplay.innerHTML = inputBuffer;
+	}
+	currentInput = "";
+	display.innerHTML = "";
+	constantEntered = false;
+}
+
+function clearInput() {
+	currentInput = "";
+	display.innerHTML = currentInput;
+}
+
+function clearAll() {
+	currentInput = "";
+	inputBuffer = [];
+	display.innerHTML = currentInput;
+	stackDisplay.innerHTML = inputBuffer;
+}
+
+function operator(action) {
+	if (currentInput != "") {
+		inputBuffer.push(currentInput);
+		currentInput = "";
+	}
+	if (inputBuffer.length > 1) {
+		number1 = parseFloat(inputBuffer.pop());
+		number2 = parseFloat(inputBuffer.pop());
+		if (action == "add") { 
+			inputBuffer.push(number2 + number1);
+		}
+		else if (action == "subtract") {
+			inputBuffer.push(number2 - number1);
+		}
+		else if (action == "multiply") {
+			inputBuffer.push(number2 * number1);
+		}
+		else if (action == "divide") {
+			inputBuffer.push(number2 / number1);
+		}
+		else if (action == "power") {
+			inputBuffer.push(Math.pow(number2, number1));
+		}
+		else if (action == "root") {
+			inputBuffer.push(Math.pow(number1, (1/number2)));
+		}
+		stackDisplay.innerHTML = inputBuffer;
+		//displayStack(); <-- this function still needs work.
+		display.innerHTML = currentInput;
+	}
+}
+
+function singleItemOperator(action) {
+	if (currentInput != "" ) {
+		inputBuffer.push(currentInput);
+		currentInput = "";
+	}
+	if (inputBuffer.length > 0) {
+		number1 = parseFloat(inputBuffer.pop());
+		if (action == "sqrt") {
+			inputBuffer.push(Math.sqrt(number1));
+		}
+		else if (action == "ln") {
+			inputBuffer.push(Math.log(number1));
+		}
+		else if (action == "log") {
+			inputBuffer.push(Math.log(number1) / Math.LN10);
+		}
+		else if (action == "sin") {
+			inputBuffer.push(Math.sin(number1));
+		}
+		else if (action == "cos") {
+			inputBuffer.push(Math.cos(number1));
+		}
+		else if (action == "tan") {
+			inputBuffer.push(Math.tan(number1));
+		}
+		else if (action == "reciprocal") {
+			inputBuffer.push(1/number1);
+		}
+		else if (action == "square") {
+			inputBuffer.push(number1 * number1);
+		}
+		else if (action == "cube") {
+			inputBuffer.push(Math.pow(number1, 3));
+		}
+		else if (action == "factorial") {
+			function factorial(numberInput) {
+				if (numberInput === 0) { 
+					return 1; 
+				}
+				else if (typeof numberInput === 'number' && numberInput % 1 === 0 && numberInput > 0) {
+					return( numberInput * factorial(numberInput - 1));
+				}
+				else {
+					
+				}
+			}
+			inputBuffer.push(factorial(number1));
+		}
+		else if (action == "10exp") {
+			inputBuffer.push(Math.pow(10, number1));
+		}
+		stackDisplay.innerHTML = inputBuffer;
+		display.innerHTML = currentInput;
+	}
+}
+
+function constant(constant) {
+	if (constant == "e") {
+		val = Math.E;
+	}
+	else if (constant == "pi") {
+		val = Math.PI;
+	}
+	else if (constant == "random") {
+		val = Math.random();
+	}
+	else if (constant == "goldenRatio") {
+		val = parseFloat(987/610);
+	}
+	if (currentInput == "") {
+		currentInput = val;
+		display.innerHTML = currentInput;
+	}
+	else {	//if number is in the buffer, multiply constant by existing number
+		currentInput = val * parseFloat(currentInput);
+		display.innerHTML = currentInput;
+	}
+	constantEntered = true; //so constant will clear form input on next non-submitted keypress
+}
+
+function popFromBuffer() {
+	if (inputBuffer.length > 0) {
+		inputBuffer.pop();
+	}
+	stackDisplay.innerHTML = inputBuffer;
+}
+
+function swap() {
+	if (inputBuffer.length > 1) {
+		number1 = inputBuffer.pop();
+		number2 = inputBuffer.pop();
+		inputBuffer.push(number1);
+		inputBuffer.push(number2);
+		stackDisplay.innerHTML = inputBuffer;
+	}
+}
+
+function changePolarity() {
+	if (currentInput != "") {
+		currentInput = -1 * parseFloat(currentInput);
+		display.innerHTML = currentInput;
+	}
+}
+
+//TODO: restrict precision of output in the stack, so entries don't display entire float values
+/*function displayStack() {
+	stackDisplay.innerHTML = "";
+	for (i = 0; i < inputBuffer.length; i++) {
+		stackDisplay.innerHTML += inputBuffer[i].toPrecision(5) + ", ";
+	}
+}*/
+
+document.onkeydown = function(e) {
+	e = e || window.event;
+    switch (e.keyCode) {
+    	case 13:
+    		addToStack();
+    		break;
+        case 48:
+        	addToBufferfromKey(0);
+        	break;
+        case 49:
+        	addToBufferfromKey(1);
+        	break;
+        case 50:
+        	addToBufferfromKey(2);
+        	break;
+        case 51:
+        	addToBufferfromKey(3);
+        	break;
+        case 52:
+        	addToBufferfromKey(4);
+        	break;
+        case 53:
+        	addToBufferfromKey(5);
+        	break;
+        case 54:
+        	addToBufferfromKey(6);
+        	break;
+        case 55:
+        	addToBufferfromKey(7);
+        	break;
+        case 56:
+        	addToBufferfromKey(8);
+        	break;
+        case 57:
+        	addToBufferfromKey(9);
+        	break;
+        case 69:
+        	constant('e');
+        	break;
+        case 88:
+        	operator("multiply");
+        	break;
+        case 173:
+        	operator("subtract");
+        	break;
+        case 191:
+        	operator("divide");
+        	break;
+    }
+};
